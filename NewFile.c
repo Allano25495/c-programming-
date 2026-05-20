12e912a1f3c6d86ac8fb053cf866e15a9a37ba5b
Allano25495 <oyaloallan9@gmail.com>
2026-05-20 10:12:59 +0300

    Add files via upload

---
+++ NewFile.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    /*
+    Description: Program displays units offered this semester
+    Name: Allan Moses Otieno Oyalo
+    Reg No: BDSA-03-0026/2026
+    Date: 13/05/2026
+    */
+
+    printf("Units offered this semester:\n");
+    printf("BCU 111 COMMUNICATION SKILLS\n");
+    printf("BCU 112 DIGITAL LITERACY\n");
+    printf("BCU 113 HEALTH AND WELLNESS MANAGEMENT\n");
+    printf("BDA DATA ETHICS, PRIVACY AND GOVERNANCE\n");
+    printf("BSD STRUCTURED PROGRAMMING AND ALGORITHMS\n");
+    printf("MAT 111 BASIC MATHEMATICS\n");
+
+    return 0;
+}
