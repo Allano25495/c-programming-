2afed1925f101590e13ef6bafd234d131e57c180
Allano25495 <oyaloallan9@gmail.com>
2026-05-28 13:03:18 +0300

    Add files via upload

---
+++ WEEK 3- Bundle Invalid Choice.c
@@ -0,0 +1,45 @@
+/*
+Program: Mobile Data Bundle Purchase
+Name: Oyalo Allan Moses Otieno
+Registration Number: BDSA-03-0026/2026
+Description: This program displays data bundle options, asks the user
+to select a bundle, and uses a switch statement to display the selected bundle and cost.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int choice;
+
+    printf("Select data bundle:\n");
+    printf("1. 100MB @ 50 KES\n");
+    printf("2. 500MB @ 200 KES\n");
+    printf("3. 1GB @ 350 KES\n");
+    printf("4. 2GB @ 600 KES\n");
+
+    printf("Enter your choice (1-4): ");
+    scanf("%d", &choice);
+
+    switch (choice) {
+        case 1:
+            printf("You selected 100MB. Cost = 50 KES\n");
+            break;
+
+        case 2:
+            printf("You selected 500MB. Cost = 200 KES\n");
+            break;
+
+        case 3:
+            printf("You selected 1GB. Cost = 350 KES\n");
+            break;
+
+        case 4:
+            printf("You selected 2GB. Cost = 600 KES\n");
+            break;
+
+        default:
+            printf("Invalid choice\n");
+    }
+
+    return 0;
+}
