854c84c354923a7f898f122ddc2a1d3ef288b130
Allano25495 <oyaloallan9@gmail.com>
2026-06-25 17:38:06 +0300

    Add files via upload

---
+++ WEEK 5-FUNCTIONS-Electric bills.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+float calculateElectricBill(int units);
+
+int main()
+{
+    int units;
+
+    printf("Enter the number of units consumed: ");
+    scanf("%d", &units);
+
+    printf("Total Electricity Bill = KSh. %.2f\n", calculateElectricBill(units));
+
+    return 0;
+}
+
+float calculateElectricBill(int units)
+{
+    float bill;
+
+    if (units <= 100)
+    {
+        bill = units * 10;
+    }
+    else if (units <= 200)
+    {
+        bill = (100 * 10) + ((units - 100) * 15);
+    }
+    else
+    {
+        bill = (100 * 10) + (100 * 15) + ((units - 200) * 20);
+    }
+
+    return bill;
+}
